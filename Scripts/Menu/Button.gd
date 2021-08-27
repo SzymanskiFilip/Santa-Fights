b82fe7ae7c0cb95dc5d78292058b98dd5e8001d9
Filip <szymanski.fp@gmail.com>
2021-08-27 22:33:38 +0200

    fixed hover glow in main menu

--- Scripts/Menu/Button.gd
+++ Scripts/Menu/Button.gd
@@ -4,7 +4,6 @@ onready var button = $"."
 
 func _process(_delta):
 	if button.is_hovered():
-		print("HOVERED")
+		pass
 	else:
-		button.add_color_override("font_color", Color(255,255,255,1))
-		print("UNHOVER")
+		pass
