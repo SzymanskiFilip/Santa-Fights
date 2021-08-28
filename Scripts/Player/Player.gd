a44e4cd62c1ab5296bc0c4a542ff9c1b56383443
Filip <szymanski.fp@gmail.com>
2021-08-28 20:46:41 +0200

    added santa attack animation

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -7,9 +7,15 @@ const UP = Vector2(0,-1)
 
 var motion = Vector2()
 
-func _physics_process(delta):
+var projectiles = 0
+
+func santa_attack():
+	if projectiles <= 2:
+		projectiles += 1
+		print("ATTACKED" + str(projectiles))
+
+func _physics_process(_delta):
 	motion.y += gravity
-	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = speed
 		$AnimatedSprite.flip_h = false
@@ -25,6 +31,8 @@ func _physics_process(delta):
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			motion.y = jump
+			
+	if Input.is_action_just_pressed("attack"):
+		santa_attack()
 	
 	motion = move_and_slide(motion, UP)
-	pass
